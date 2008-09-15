cf27095b0ee496acb354a02817bd8e0d39646f92
Ernesto Rico Schmidt <e.rico.schmidt@gmail.com>
2008-09-15 16:57:43 -0400

    added base settings for project.

---
+++ settings.py
@@ -0,0 +1,59 @@
+import os
+
+basedir = os.path.dirname(__file__)
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASE_ENGINE = 'sqlite3'
+DATABASE_NAME = os.path.join(basedir, 'solmujeres.sqlite')
+
+TIME_ZONE = 'America/La_Paz'
+
+LANGUAGE_CODE = 'es'
+
+SITE_ID = 1
+
+USE_I18N = True
+
+MEDIA_ROOT = ''
+
+MEDIA_URL = ''
+
+ADMIN_MEDIA_PREFIX = '/media/'
+
+SECRET_KEY = 'o*)z&e$p)w))pv!m^umqrk98wk1f)!y_%qadsz-$#qyfckr2(#'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+#     'django.template.loaders.eggs.load_template_source',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+)
+
+ROOT_URLCONF = 'solmujeres.urls'
+
+TEMPLATE_DIRS = (os.path.join(basedir, 'templates'),)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.flatpages',
+    'django.contrib.markup',
+    'django.contrib.syndication',
+)
